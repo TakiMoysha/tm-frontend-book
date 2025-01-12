d12b8ec47dd7bdc9af27b5dc51b56e681ba79f56
TakiMoysha <mpwema782@gmail.com>
2025-01-12 18:01:43 +0100

    update to producation

---
+++ vue-order-book/components/OrderBook.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  <div>OrderBook</div>
+</template>
--- vue-order-book/pages/index.vue
+++ vue-order-book/pages/index.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import SideBar from "~/components/SideBar.vue";
+// import SideBar from "~/components/SideBar.vue";
 import OrderBook from "~/components/OrderBook.vue";
 </script>
 
 <template>
   <div>
-    <SideBar></SideBar>
+    <!-- <SideBar></SideBar> -->
     <OrderBook></OrderBook>
   </div>
 </template>
