1d83018a7ffff7254616f6b2466bfc4fe4c2d81d
TakiMoysha <mpwema782@gmail.com>
2025-01-12 17:40:41 +0100

    update vue-order-book

---
+++ vue-order-book/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import SideBar from "~/components/SideBar.vue";
+import OrderBook from "~/components/OrderBook.vue";
+</script>
+
+<template>
+  <div>
+    <SideBar></SideBar>
+    <OrderBook></OrderBook>
+  </div>
+</template>
+
+<style scoped></style>
